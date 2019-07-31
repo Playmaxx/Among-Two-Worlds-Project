d91a1e937c75cea2f4e09a1efdffff7177ca5302
BRD <fbardia9@gmail.com>
2019-07-31 17:21:11 +0200

    final commit 31.07.

---
+++ Among 2 Worlds/Assets/Scripts/PopUpPanels.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpPanels : MonoBehaviour
+{
+    public RectTransform panel_jump;
+    public CanvasGroup canvasGroup;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //canvasGroup = GetComponent<CanvasGroup>
+        panel_jump = GetComponent<RectTransform>();
+    }
+
+    void Hide()
+    {
+        canvasGroup.alpha = 0f; //this makes everything transparent
+        canvasGroup.blocksRaycasts = false; //this prevents the UI element to receive input events
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
