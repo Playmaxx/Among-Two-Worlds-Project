ff9834ecf44d27be02fde98d9f73597787720a2c
BRD <fbardia9@gmail.com>
2019-08-01 15:03:28 +0200

    final commit 01.08. (PopUp edits)

---
+++ Among 2 Worlds/Assets/Scripts/PopUps.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUps : MonoBehaviour
+{
+    public GameObject panelRef;
+    bool state = false;
+
+    void Awake()
+    {
+        panelRef = gameObject.transform.Find("Panel(Jump)").gameObject;
+    }
+    
+    void Update()
+    {
+        panelRef.SetActive(state);
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            state = !state;
+        }
+
+        //if (Input.GetKeyDown(KeyCode.O))
+        //{
+            //panelRef = gameObject.transform.Find("Panel(Worldshifting)").gameObject;
+            //state = !state;
+        //}
+    }
+}
