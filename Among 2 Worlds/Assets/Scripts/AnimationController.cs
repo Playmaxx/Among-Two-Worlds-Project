dd22f708ab4f06ef142a43e241fc75afee6c02d1
Simon <marx.simon@gmx.de>
2019-08-01 15:48:08 +0200

    FINALY GOT THAT FCKNGADNOFA ANIMATION

---
+++ Among 2 Worlds/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+
+    Player playerRef;
+    private Animator animator;
+
+    //Awake is called before Start.
+    private void Awake()
+    {
+        playerRef = GetComponent<Player>();
+        animator = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(playerRef.rigidRef.velocity.x > 0)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float characterSpeed = Mathf.Abs(playerRef.rigidRef.velocity.x);
+        animator.SetFloat("Speed", characterSpeed);
+    
+    }
+}
