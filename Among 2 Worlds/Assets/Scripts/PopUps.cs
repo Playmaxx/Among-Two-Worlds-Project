fbc4f35f4eee1ae37d6fc03fc2ee805b3fa7484a
BRD <fbardia9@gmail.com>
2019-08-02 10:22:30 +0200

    PopUp edits

--- Among 2 Worlds/Assets/Scripts/PopUps.cs
+++ Among 2 Worlds/Assets/Scripts/PopUps.cs
@@ -4,26 +4,35 @@ using UnityEngine;
 
 public class PopUps : MonoBehaviour
 {
+    GameObject[] panels;
     public GameObject panelRef;
     bool state = false;
 
     void Awake()
     {
-        panelRef = gameObject.transform.Find("Panel(Jump)").gameObject;
+        panels = new GameObject[2];
+        panels[0] = gameObject.transform.Find("Panel(Jump)").gameObject;
+        panels[1] = gameObject.transform.Find("Panel(Worldshifting)").gameObject;
+
+        foreach(GameObject panel in panels)
+        {
+            panel.SetActive(state);
+        }
     }
     
     void Update()
     {
-        panelRef.SetActive(state);
+        //panelRef.SetActive(state);
         if (Input.GetKeyDown(KeyCode.P))
         {
+            panelRef = gameObject.transform.Find("Panel(Jump)").gameObject;
             state = !state;
         }
 
-        //if (Input.GetKeyDown(KeyCode.O))
-        //{
-            //panelRef = gameObject.transform.Find("Panel(Worldshifting)").gameObject;
-            //state = !state;
-        //}
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            panelRef = gameObject.transform.Find("Panel(Worldshifting)").gameObject;
+            state = !state;
+        }
     }
 }
