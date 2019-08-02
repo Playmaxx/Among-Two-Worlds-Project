d71bcb86e62b1defb40957461ddbed4f55d0f89f
Simon <marx.simon@gmx.de>
2019-08-02 19:04:03 +0200

    Animation Work

--- Among 2 Worlds/Assets/Scripts/AnimationController.cs
+++ Among 2 Worlds/Assets/Scripts/AnimationController.cs
@@ -4,28 +4,80 @@ using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
-
+    GameManager gameManagerRef;
     Player playerRef;
     private Animator animator;
 
     //Awake is called before Start.
     private void Awake()
     {
+        gameManagerRef = GetComponent<GameManager>();
         playerRef = GetComponent<Player>();
         animator = GetComponent<Animator>();
     }
     // Start is called before the first frame update
     void Start()
     {
-  
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        CheckState();
+    }
 
+    void CheckState() //Checks the different Variables necessery for Animations
+    {
+
+        bool isGrounded;  //Checks if player is Grounded
+        if (playerRef.playerMoveState == Player.moveState.Grounded)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        animator.SetBool("isGrounded", isGrounded);
+
+        bool isJumping;   //Checks if player is Jumping
+        if (playerRef.playerMoveState == Player.moveState.Jumping)
+        {
+            isJumping = true;
+        }
+        else
+        {
+            isJumping = false;
+        }
+        animator.SetBool("isJumping", isJumping);
+
+        bool isGliding;   //Checks if player is Gliding
+        if (playerRef.playerMoveState == Player.moveState.Gliding)
+        {
+            isGliding = true;
+        }
+        else
+        {
+            isGliding = false;
+        }
+        animator.SetBool("isGliding", isGliding);
+
+        bool LightDimension;   //Checks which Dimension is active right now
+        if (gameManagerRef.currentdim == GameManager.dimension.Light)
+        {
+            LightDimension = true;
+        }
+        else
+        {
+            LightDimension = false;
+        }
+        animator.SetBool("LightDimension", LightDimension);
+
+
+
+        //Checks if the character is moving
         float characterSpeed = Mathf.Abs(playerRef.rigidRef.velocity.x);
         animator.SetFloat("Speed", characterSpeed);
-    
     }
 }
