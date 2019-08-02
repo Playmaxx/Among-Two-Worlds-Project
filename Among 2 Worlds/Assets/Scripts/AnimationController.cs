66126c817ba918ab37a6c665a0609d8899ad8353
Simon <marx.simon@gmx.de>
2019-08-02 14:34:22 +0200

    Update Animation

--- Among 2 Worlds/Assets/Scripts/AnimationController.cs
+++ Among 2 Worlds/Assets/Scripts/AnimationController.cs
@@ -17,10 +17,7 @@ public class AnimationController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if(playerRef.rigidRef.velocity.x > 0)
-        {
-
-        }
+  
     }
 
     // Update is called once per frame
