244b8f2e335656db3a5b735dc00ebc15ba2d01ba
MATOO <fagoldob@gmail.com>
2019-08-06 17:26:24 +0200

    Added empty scripts

---
+++ Among 2 Worlds/Assets/Scripts/Werewolf.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Werewolf : MonoBehaviour
+{
+    int leftPatrolX;
+    int rightPatrolX;
+    int attackRange;
+    bool playerTargeted = false;
+
+    Player playerRef;
+
+    void Awake()
+    {
+        playerRef = GetComponent<Player>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        closeDistance();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerTargeted = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerTargeted = false;
+        }
+    }
+
+    void closeDistance()
+    {
+        if(playerTargeted == true)
+        {
+
+        }
+    }
+}
