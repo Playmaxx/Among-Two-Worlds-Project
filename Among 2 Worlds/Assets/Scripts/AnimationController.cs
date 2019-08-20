6cd346ff1ad26894e80b556ad0179a457cbf412d
BRD <fbardia9@gmail.com>
2019-08-20 15:17:59 +0200

    restore 2 - return of the restore

---
+++ Among 2 Worlds/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+   
+    Player playerRef;
+    public Animator animator;
+
+    //Awake is called before Start.
+    private void Awake()
+    {
+       
+        playerRef = GetComponent<Player>();
+        animator = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckState();
+    }
+
+    void CheckState() //Checks the different Variables necessery for Animations
+    {
+        bool LightDimension;   //Checks which Dimension is active right now
+        if (GameManager.GMInstance.currentdim == GameManager.dimension.Light)
+        {
+            LightDimension = true;
+        }
+        else
+        {
+            LightDimension = false;
+        }
+        animator.SetBool("LightDimension", LightDimension);
+
+
+        bool isGrounded;  //Checks if player is Grounded
+        if (playerRef.playerMoveState == Player.moveState.Grounded)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        animator.SetBool("isGrounded", isGrounded);
+
+        bool isJumping;   //Checks if player is Jumping
+        if (playerRef.playerMoveState == Player.moveState.Jumping)
+        {
+            isJumping = true;
+        }
+        else
+        {
+            isJumping = false;
+        }
+        animator.SetBool("isJumping", isJumping);
+
+        bool isGliding;   //Checks if player is Gliding
+        if (playerRef.playerMoveState == Player.moveState.Gliding)
+        {
+            isGliding = true;
+        }
+        else
+        {
+            isGliding = false;
+        }
+        animator.SetBool("isGliding", isGliding);
+
+
+        bool isFalling;    //Checks if player is falling down
+        if (playerRef.playerMoveState == Player.moveState.Falling)
+        {
+            isFalling = true;
+        }
+        else
+        {
+            isFalling = false;
+        }
+        animator.SetBool("isFalling", isFalling);
+
+        bool isWalled;
+        if(playerRef.playerMoveState == Player.moveState.Walled)
+        {
+            isWalled = true;
+        }
+        else
+        {
+            isWalled = false;
+        }
+        animator.SetBool("isWalled", isWalled);
+
+        bool isDashing;
+        if(playerRef.playerMoveState == Player.moveState.Dashing)
+        {
+            isDashing = true;
+        }
+        else
+        {
+            isDashing = false;
+        }
+        animator.SetBool("isDashing", isDashing);
+
+
+        int NumberOfJumps = playerRef.Jumps;   //Check needed for doublejumping
+        animator.SetInteger("NumberOfJumps", NumberOfJumps);
+
+        //Checks if the character is moving
+        float characterSpeed = Mathf.Abs(playerRef.rigidRef.velocity.x);
+        animator.SetFloat("Speed", characterSpeed);
+    }
+}
---
+++ Among 2 Worlds/Assets/Scripts/Audio/SFXManager.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    [SerializeField]
+    Player playerRef;
+    Rigidbody2D rigidRef;
+
+    public AudioSource current_audioclip;
+    public AudioSource current_audioclip2;
+    public AudioClip Jump;
+    public AudioClip Dash;
+    public AudioClip KnightGettingHit;
+
+    [SerializeField]
+    bool JumpPlayed = false;
+    [SerializeField]
+    bool DashPlayed = false;
+
+    //Awake is called before start
+    void Awake()
+    {
+        playerRef = FindObjectOfType<Player>();
+        current_audioclip = GetComponent<AudioSource>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        current_audioclip.volume = 1.0f;
+        current_audioclip.clip = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Call_PlayJumpSound();
+        Call_PlayDashSound();
+    }
+
+    public void playJumpSound()
+    {
+        current_audioclip2.clip = Jump;
+        current_audioclip2.Play();
+        Debug.Log("Jump-Sound-test");
+    }
+
+    public void Call_PlayJumpSound()
+    {
+        if (GameManager.GMInstance.currentdim == GameManager.dimension.Dark)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && JumpPlayed == false)
+            {
+                playJumpSound();
+                JumpPlayed = true;
+                Debug.Log(JumpPlayed);
+            }
+        }
+
+        if (GameManager.GMInstance.currentdim == GameManager.dimension.Light)
+        {
+            if (playerRef.rigidRef.velocity.y > 0 && JumpPlayed == false)
+            {
+                playJumpSound();
+                JumpPlayed = true;
+                Debug.Log(JumpPlayed);
+            }
+        }
+        
+        if (playerRef.playerMoveState == Player.moveState.Grounded)
+        {
+            JumpPlayed = false;
+        }
+    }
+
+
+    public void playDashSound()
+    {
+            current_audioclip.clip = Dash;
+            current_audioclip.Play();
+            Debug.Log("dash sound test");
+    }
+    
+    public void Call_PlayDashSound()
+    {
+        if ((GameManager.GMInstance.currentdim == GameManager.dimension.Dark))
+        {
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+            {
+                if (Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                    if (DashPlayed == false)
+                    playDashSound();
+                    DashPlayed = true;
+                }
+            }
+        }
+
+        if (playerRef.playerMoveState != Player.moveState.Dashing)
+        {
+            DashPlayed = false;
+        }
+    }
+    public void playKnightGettingHitSound()
+    {
+        current_audioclip.clip = KnightGettingHit;
+        current_audioclip.Play();
+        Debug.Log("KnightGitHit");
+    }
+}
