8fd055dd2196412d3e3864d8cd5b76b8c43bd473
Simon <marx.simon@gmx.de>
2019-08-22 12:10:06 +0200

    blubb

---
+++ Among 2 Worlds/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+
+    private Player PlayerRef;
+
+
+    private void Awake()
+    {
+        PlayerRef = GameObject.FindWithTag("Player").GetComponent<Player>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Test");
+        if(other.tag == "Player")
+        {
+            PlayerRef.playerMoveState = Player.moveState.Other;
+            Debug.Log("Test2");
+        }
+    }
+
+
+}
