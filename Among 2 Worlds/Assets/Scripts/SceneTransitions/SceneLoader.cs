c94573b311c8c467c477649baeed0c1c1194a3cf
BRD <fbardia9@gmail.com>
2019-08-23 14:45:11 +0200

    new popup bg added, scene switch edits & bugfixes

---
+++ Among 2 Worlds/Assets/Scripts/SceneTransitions/SceneLoader.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public enum targetScene { Eingang, Haupthalle, Keller, Türme, Gang, Boss }
+    public targetScene t_Scene;
+    
+    public int targetX;
+    public int targetY;
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && (Input.GetKeyDown(KeyCode.Q) || Input.GetButtonDown("Interact")))
+        {
+            loadScene();
+        }
+    }
+
+    void loadScene()
+    {
+        GameManager.GMInstance.respawnX = targetX;
+        GameManager.GMInstance.respawnY = targetY;
+        Debug.Log(GameManager.GMInstance.respawnX);
+        Debug.Log(GameManager.GMInstance.respawnY);
+
+        switch (t_Scene)
+        {
+            case (targetScene.Eingang):
+                GameManager.GMInstance.currentlvl = GameManager.level.Eingang;
+                SceneManager.LoadScene("Level 1");
+                break;
+
+            case (targetScene.Haupthalle):
+                GameManager.GMInstance.currentlvl = GameManager.level.Haupthalle;
+                SceneManager.LoadScene("Level 2");
+                break;
+
+            case (targetScene.Keller):
+                GameManager.GMInstance.currentlvl = GameManager.level.Keller;
+                SceneManager.LoadScene("Level 3");
+                break;
+
+            case (targetScene.Türme):
+                GameManager.GMInstance.currentlvl = GameManager.level.Türme;
+                SceneManager.LoadScene("Level 4");
+                break;
+
+            case (targetScene.Gang):
+                GameManager.GMInstance.currentlvl = GameManager.level.Gang;
+                SceneManager.LoadScene("Level 5");
+                break;
+
+            case (targetScene.Boss):
+                GameManager.GMInstance.currentlvl = GameManager.level.Boss;
+                SceneManager.LoadScene("Boss");
+                break;
+        }
+    }
+}
