ece4046fc2470e8938b4b9723b498caaae2c5951
MATOO <fagoldob@gmail.com>
2019-08-23 17:03:02 +0200

    added basic archer (tracks player)

--- Among 2 Worlds/Assets/Scripts/Enemys/Archer.cs
+++ Among 2 Worlds/Assets/Scripts/Enemys/Archer.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class Archer : MonoBehaviour
 {
+    int health = 100;
+    float trackingdistance;
+    bool playerInRange = false;
+    bool playerTracked = false;
+    Player playerRef;
+
+    void Awake()
+    {
+        playerRef = GameObject.FindWithTag("Player").GetComponent<Player>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        trackingdistance = GetComponent<CircleCollider2D>().radius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerInRange == false)
+        {
+            Debug.DrawLine(transform.position, playerRef.transform.position, Color.green);
+        }
+        if (playerInRange == true && playerTracked == false)
+        {
+            Debug.DrawLine(transform.position, playerRef.transform.position, new Color(1.0f, 0.64f, 0.0f));
+        }
+        if (playerInRange == true && playerTracked == true)
+        {
+            Debug.DrawLine(transform.position, playerRef.transform.position, Color.red);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (playerInRange == true)
+        {
+            if(Physics2D.Raycast(transform.position, playerRef.transform.position.normalized, trackingdistance, GameManager.GMInstance.platformMask))
+            {
+                playerTracked = false;
+            }
+            if (!Physics2D.Raycast(transform.position, playerRef.transform.position.normalized, trackingdistance, GameManager.GMInstance.platformMask))
+            {
+                playerTracked = true;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerInRange = false;
+            playerTracked = false;
+        }
+    }
+
+    void shoot()
+    {
+
     }
 }
