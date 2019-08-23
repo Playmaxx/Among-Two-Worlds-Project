9c34a52bc29a47906642458d374b5ad4e09fe53a
Simon <marx.simon@gmx.de>
2019-08-23 17:34:09 +0200

    Added Scripts for Levers

---
+++ Among 2 Worlds/Assets/Scripts/Other/DungeonLever.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonLever : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.GMInstance.DungeonLever == true)
+        {
+            //flip sprite
+            Destroy(this);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            GameManager.GMInstance.DungeonLever = true;
+        }
+    }
+
+}
