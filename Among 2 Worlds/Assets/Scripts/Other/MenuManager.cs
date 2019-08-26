971f599fdab6b5248eb0b5ccf13d3b50740cea4f
BRD <fbardia9@gmail.com>
2019-08-26 18:38:35 +0200

    Main Menu & Sound polishing

--- Among 2 Worlds/Assets/Scripts/Other/MenuManager.cs
+++ Among 2 Worlds/Assets/Scripts/Other/MenuManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
@@ -21,12 +20,15 @@ public class MenuManager : MonoBehaviour
     public AudioMixer SFX_audioMixer;
     public AudioMixer Step_audioMixer;
     public AudioMixer rattle_audioMixer;
+    public AudioMixer jump_audioMixer;
+    public AudioMixer knightWalk_audioMixer;
+    public AudioMixer menu_audioMixer;
 
     public Toggle toggleMusic;
 
     public void StartGame()
     {
-        SceneManager.LoadScene("Level 1");
+        SceneManager.LoadScene("LVL1_Test_Bardia");
     }
 
     public void LoadNewPanel()
@@ -44,6 +46,7 @@ public class MenuManager : MonoBehaviour
     public void SetMusicVolume(float volume)
     {
         BGM_audioMixer.SetFloat("Volume", volume);
+        menu_audioMixer.SetFloat("MenuVolume", volume);
     }
 
     public void PlayMusicToTest()
@@ -58,9 +61,11 @@ public class MenuManager : MonoBehaviour
 
     public void SetSFXVolume(float volume)
     {
-        SFX_audioMixer.SetFloat("SFXVolume", volume);
+        SFX_audioMixer.SetFloat("TrueSFXVolume", volume);
         Step_audioMixer.SetFloat("StepVolume", volume);
         rattle_audioMixer.SetFloat("RattleVolume", volume);
+        jump_audioMixer.SetFloat("SFXVolume", volume);
+        knightWalk_audioMixer.SetFloat("KnightWalkVolume", volume);
     }
 
     public void PlaySFXToTest()
