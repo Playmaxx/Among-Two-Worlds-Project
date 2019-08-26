8abc4e2c34e7c5eca6c61141b592437a58245e07
BRD <fbardia9@gmail.com>
2019-08-26 19:35:47 +0200

    final commit 26.08. (Sound-polishing)

---
+++ Among 2 Worlds/Assets/Scripts/Other/DontDestroySFX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroySFX : MonoBehaviour
+{
+    private static DontDestroySFX instance = null;
+    public static DontDestroySFX instance2
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
